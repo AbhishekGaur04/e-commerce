864167955eb5327300e96980e1f58cf8a53a37ec
Abhishek-Gaur <135010746+AbhishekGaur04@users.noreply.github.com>
2025-07-19 20:22:09 +0530

    Update database.py

--- config/database.py
+++ config/database.py
@@ -5,6 +5,7 @@ import os
 load_dotenv()
 
 MONGO_URI = os.getenv("MONGO_URI")
-
+if not MONGO_URI:
+    raise Exception("MONGO_URI not found in env!")
 client = AsyncIOMotorClient(MONGO_URI)
 database = client.ecommerce
